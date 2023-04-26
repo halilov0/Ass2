62f06aafb80b086606044ede483360c4388c96ea
halilov0 <128397153+halilov0@users.noreply.github.com>
2023-04-26 23:49:14 +0300

    Add files via upload

---
+++ DT_Assignment2/src/Container.java
@@ -0,0 +1,12 @@
+
+//Don't change the class name
+public class Container{
+    private Point data;//Don't delete or change this field;
+
+    //Don't delete or change this function
+    public Point getData()
+    {
+        System.out.println("g");
+        return data;
+    }
+}
