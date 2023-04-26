7be217f2ddbffa6eafd60cf49a7efd5cc04458aa
Idan Halilov <halilovi@post.bgu.ac.il>
2023-04-27 00:31:00 +0300

    GL

--- DT_Assignment2/src/DataStructure.java
+++ DT_Assignment2/src/DataStructure.java
@@ -1,22 +1,38 @@
 
 public class DataStructure implements DT {
 
+    private DynamicArray<Point> xAxis;
+    private DynamicArray<Point> yAxis;
 
     //////////////// DON'T DELETE THIS CONSTRUCTOR ////////////////
     public DataStructure()
     {
-        //TODO
+        xAxis = new DynamicArray<>();
+        yAxis = new DynamicArray<>();
     }
 
     @Override
     public void addPoint(Point point) {
-        // TODO Auto-generated method stub
+        planes.add(point);
+    }
 
+    public void addXPoint(Point point) {
+        if(xAxis.isEmpty())
+            xAxis.add(point);
+        else {
+            for(int i=0; i<xAxis.size(); i=i+1)
+                if(xAxis.get(i).getX() > point.getX())
+                    xAxis.add(i,point);
+        }
+    }
+
+    public void addYPoint(Point point) {
+        planes.add(point);
     }
 
     @Override
     public Point[] getPointsInRangeRegAxis(int min, int max, Boolean axis) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
