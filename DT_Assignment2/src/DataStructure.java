72f5cea439d785187fd857586ed83d022dbaef32
Idan Halilov <halilovi@post.bgu.ac.il>
2023-04-27 01:13:17 +0300

    lets call it a day

--- DT_Assignment2/src/DataStructure.java
+++ DT_Assignment2/src/DataStructure.java
@@ -13,7 +13,8 @@ public class DataStructure implements DT {
 
     @Override
     public void addPoint(Point point) {
-        planes.add(point);
+        addXPoint(point);
+        addYPoint(point);
     }
 
     public void addXPoint(Point point) {
@@ -27,30 +28,91 @@ public class DataStructure implements DT {
     }
 
     public void addYPoint(Point point) {
-        planes.add(point);
+        if (yAxis.isEmpty())
+            yAxis.add(point);
+        else {
+            for (int i = 0; i < yAxis.size(); i = i + 1)
+                if (yAxis.get(i).getX() > point.getX())
+                    yAxis.add(i, point);
+        }
     }
 
     @Override
     public Point[] getPointsInRangeRegAxis(int min, int max, Boolean axis) {
+        DynamicArray<Point> smartArray = new DynamicArray<>();
+        boolean stop = false;
+        if(axis){
+            for(int i=0; i<xAxis.size() && !stop; i=i+1){
+                if(xAxis.get(i).getX()>=min && xAxis.get(i).getX()<=max) {
+                    smartArray.add(xAxis.get(i));
+                }
+                else if (xAxis.get(i).getX()>max)
+                    stop = true;
+            }
+        }
+        else{
+            for(int i=0; i<yAxis.size() && !stop; i=i+1){
+                if(yAxis.get(i).getX()>=min && yAxis.get(i).getX()<=max) {
+                    smartArray.add(yAxis.get(i));
+                }
+                else if (yAxis.get(i).getX()>max)
+                    stop = true;
+            }
 
-        return null;
+        }
+        Point[] array = new Point[smartArray.size()];
+        for(int i=0; i<array.length; i=i+1){
+            array[i] = smartArray.get(i);
+        }
+        return array;
     }
 
     @Override
     public Point[] getPointsInRangeOppAxis(int min, int max, Boolean axis) {
-        // TODO Auto-generated method stub
-        return null;
+        DynamicArray<Point> smartArray = new DynamicArray<>();
+        boolean stop = false;
+        if(!axis){
+            for(int i=0; i<xAxis.size() && !stop; i=i+1){
+                if(xAxis.get(i).getX()>=min && xAxis.get(i).getX()<=max) {
+                    smartArray.add(xAxis.get(i));
+                }
+                else if (xAxis.get(i).getX()>max)
+                    stop = true;
+            }
+        }
+        else{
+            for(int i=0; i<yAxis.size() && !stop; i=i+1){
+                if(yAxis.get(i).getX()>=min && yAxis.get(i).getX()<=max) {
+                    smartArray.add(yAxis.get(i));
+                }
+                else if (yAxis.get(i).getX()>max)
+                    stop = true;
+            }
+
+        }
+        Point[] array = new Point[smartArray.size()];
+        for(int i=0; i<array.length; i=i+1){
+            array[i] = smartArray.get(i);
+        }
+        return array;
     }
 
     @Override
     public double getDensity() {
-        // TODO Auto-generated method stub
-        return 0;
+        double ans;
+        int n = xAxis.size();
+        int xMin = xAxis.get(0).getX();
+        int xMax = xAxis.get(n-1).getX();
+        int yMin = yAxis.get(0).getY();
+        int yMax = yAxis.get(n-1).getY();
+        int disX = xMax - xMin;
+        int disY = yMax - yMin;
+        ans = n/(disX*disY);
+        return ans;
     }
 
     @Override
     public void narrowRange(int min, int max, Boolean axis) {
-        // TODO Auto-generated method stub
 
     }
 
